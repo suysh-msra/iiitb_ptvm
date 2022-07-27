79b67a2ec277a28d0079e1a9953e09cbaff2fbaf
suysh-msra <84946358+suysh-msra@users.noreply.github.com>
2022-07-27 10:20:35 +0530

    Update iiitb_ptvm.v

--- iiitb_ptvm.v
+++ iiitb_ptvm.v
@@ -1,5 +1,5 @@
 module iiitb_fsm(clock,reset,coin,vend,state,change);
-\\these are the inputs and the outputs.
+//\\these are the inputs and the outputs.
 input clock;
 input reset;
 input [2:0]coin;
@@ -8,27 +8,27 @@ output [2:0]state;
 output [2:0]change;
 
   
-  \\i need to define the registers as change,coin and vend
+ // \\i need to define the registers as change,coin and vend
 reg vend;
 reg [2:0]change;
 wire [2:0]coin;
-\\my coins are declared as parameters to make reading better.
+//\\my coins are declared as parameters to make reading better.
 parameter [2:0]NICKEL=3’b001;
 parameter [2:0]DIME=3’b010;
 parameter [2:0]NICKEL_DIME=3’b011;
 parameter [2:0]DIME_DIME=3’b100;
 parameter [2:0]QUARTER=3’b101;
-\\MY STATES ARE ALSO PARAMETERS . I DONT WANT TO MAKE YOU READ
-\\IN MACHINE LANGUAGE
+//\\MY STATES ARE ALSO PARAMETERS . I DONT WANT TO MAKE YOU READ
+//\\IN MACHINE LANGUAGE
 parameter [2:0]IDLE=3’b000;
 parameter [2:0]FIVE=3’b001;
 parameter [2:0]TEN=3’b010;
 parameter [2:0]FIFTEEN=3’b011;
 parameter [2:0]TWENTY=3’b100;
 parameter [2:0]TWENTYFIVE=3’b101;
-\\AS ALWAYS THE STATES ARE DEFINED AS REG
+//\\AS ALWAYS THE STATES ARE DEFINED AS REG
 reg [2:0]state,next_state;
-\\MY MACHINE WORKS ON STATE AND COIN
+//\\MY MACHINE WORKS ON STATE AND COIN
 always @(state or coin)
 begin
 next_state=0; \\VERYFIRST NEXT STATE IS GIVEN ZERO
@@ -68,15 +68,15 @@ default : next_state=IDLE;
 endcase
 end
 always @(clock)
-  begin \\WHENEVER I GIVE A RESET I HAVE TO MAKE THE STATE TO IDLE AND VEND TO 1
+  begin //\\WHENEVER I GIVE A RESET I HAVE TO MAKE THE STATE TO IDLE AND VEND TO 1
 if(reset) begin
 state <= IDLE;
 vend <= 1’b0;
 // change <= 3’b000;
-end \\THE CHANGE ALSO HAS TO BECOME NONE
+end //\\THE CHANGE ALSO HAS TO BECOME NONE
 else state <= next_state;
     case (state) \\HERE WE DECIDE THE NEXT STATE
-\\ALL THE STATES ARE DEFINED HERE AND THE OUTPUT IS ALSO GIVEN
+//\\ALL THE STATES ARE DEFINED HERE AND THE OUTPUT IS ALSO GIVEN
 IDLE: begin vend <= 1’b0; change <=3’d0; end
 FIVE: begin vend <= 1’b0; if (coin==QUARTER) change <=NICKEL; else change <=3’dTEN: begin vend <= 1’b0; if (coin==QUARTER) change <=DIME; else change <= 3’dFIFTEEN : begin vend <= 1’b0; if (coin==QUARTER) change <=NICKEL_DIME; else chaTWENTY : begin vend <= 1’b0; if (coin==DIME) change <=NICKEL; else if (coin==QUTWENTYFIVE : begin vend <= 1’b1; change <=3’d0; end
 default: state <= IDLE;
