451064b39c5dab34178e1f36ff6a23c116943c8a
suysh-msra <84946358+suysh-msra@users.noreply.github.com>
2022-07-28 10:55:08 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -5,10 +5,9 @@ read_verilog iiitb_ptvm.v
 # generic synthesis
 synth -top iiitb_ptvm
 # mapping to mycells.lib
-dfflibmap -liberty /home/suysh.msra/iiitb_ptvm/tree/main/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+dfflibmap -liberty /home/suysh.msra/iiitb_ptvm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 
-abc -liberty /home/suysh.msra/iiitb_ptvm/tree/main/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-
+abc -liberty  /home/suysh.msra/iiitb_ptvm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
 flatten
 # write synthesized design
