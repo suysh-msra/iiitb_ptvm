8d33bb81d59269dc0666431c4596d25e8b5209be
suysh-msra <84946358+suysh-msra@users.noreply.github.com>
2022-07-28 10:33:22 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -5,7 +5,8 @@ read_verilog iiitb_ptvm.v
 # generic synthesis
 synth -top iiitb_ptvm
 # mapping to mycells.lib
-dfflibmap -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
+dfflibmap -liberty /home/suysh.msra/iiitb_ptvm/tree/main/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+
 abc -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
 clean
 flatten
